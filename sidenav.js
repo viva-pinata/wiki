9a7184452ba2b9433dd19390e23eef16995a1def
viva-pinata <theglitchedsalamango@gmail.com>
2024-06-07 01:52:47 +0200

    fix opacity

--- sidenav.js
+++ sidenav.js
@@ -1,9 +1,7 @@
 function openNav() {
     document.getElementById("sideNav").style.width = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
   
   function closeNav() {
     document.getElementById("sideNav").style.width = "0";
-    document.body.style.backgroundColor = "white";
   }
